42167d081ef2fdcaa066e058a64646a4e574ca46
Ivan Buccella <ivan@buccella.me>
2021-11-26 02:55:44 +0100

    Added shortest path algorithm.

--- src/backend/app/Http/Controllers/FlightsController.php
+++ src/backend/app/Http/Controllers/FlightsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Airport;
 use App\Models\Flight;
 
 class FlightsController extends Controller
 {
+
     /**
      * Create a new controller instance.
      *
@@ -23,8 +25,51 @@ class FlightsController extends Controller
      */
     public function getLowestPriceFlight()
     {
-        $flights = Flight::all();
-        return response()->json($flights);
+        /*
+        foreach (Airport::all() as $airport) {
+        $flights = Flight::where('code_departure', $airport->id)->get();
+        //::has('departure', '=', $airport->id)->get(); //For ogni arco (v,w) ∈ E
+        $a = $airport->code;
+        foreach ($flights as $flight) {
+        $b = $flight->departure->code;
+        $c = $flight->arrival->code;
+        $d = $flight->price;
+        }
+        }
+         */
+        $a = $this->shortestPath(1, 3, 2);
+
+        return response()->json($a);
+    }
+
+    private function shortestPath($s, $t, $maxDepth)
+    {
+        $infinite = 0x7FFFFFFF;
+
+        $airports = Airport::all();
+
+        $m = [[]];
+        foreach ($airports as $airport) {
+            $m[0][$airport->id] = $infinite;
+        }
+        $m[0][$t] = 0;
+
+        for ($i = 1; $i < $maxDepth; $i++) {
+            foreach ($airports as $airport) {
+                $v         = $airport->id;
+                $previous  = $i - 1;
+                $m[$i][$v] = $m[$previous][$v];
+                $flights   = Flight::where('code_departure', $v)->get(); //For ogni arco (v,w) ∈ E
+                foreach ($flights as $flight) {
+                    $w = $flight->arrival->id;
+                    if ($m[$i][$v] > $m[$previous][$w] + $flight->price) {
+                        $m[$i][$v] = $m[$previous][$w] + $flight->price;
+                    }
+                }
+            }
+        }
+
+        return $m[$maxDepth - 1][$s];
     }
 
 }
